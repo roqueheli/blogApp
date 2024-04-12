eeb804deb348be5062a16d1ee3d663fae2e4a386
Roqueheli <roqueheliarenas@gmail.com>
2024-04-12 10:00:56 -0400

    excercises from 4-4 to 4-5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,24 @@ const dummy = (blogs) => {
   return blogs?.length === 0 ? 1 : blogs.length;
 };
 
+const totalLikes = (blogs) => {      
+  return blogs?.length === 0
+    ? 0
+    : blogs.reduce((accum, blogs) => {
+      return accum + blogs.likes;
+    }, 0);
+};
+
+const favoriteBlog = (blogs) => {      
+    return blogs?.length === 0
+      ? 0
+      : blogs.reduce((accum, blog) => {
+        return (blog.likes > accum.likes) ? blog : accum
+      });
+  };
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
