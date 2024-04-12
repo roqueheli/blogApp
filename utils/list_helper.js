89ca3be5b863830a2da5d139d477e82d207ca736
Roqueheli <roqueheliarenas@gmail.com>
2024-04-12 11:51:49 -0400

    excercise part4-6 lodash

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,25 +1,51 @@
+const _ = require("lodash");
+
 const dummy = (blogs) => {
   return blogs?.length === 0 ? 1 : blogs.length;
 };
 
-const totalLikes = (blogs) => {      
+const totalLikes = (blogs) => {
   return blogs?.length === 0
     ? 0
     : blogs.reduce((accum, blogs) => {
-      return accum + blogs.likes;
-    }, 0);
+        return accum + blogs.likes;
+      }, 0);
 };
 
-const favoriteBlog = (blogs) => {      
-    return blogs?.length === 0
-      ? 0
-      : blogs.reduce((accum, blog) => {
-        return (blog.likes > accum.likes) ? blog : accum
+const favoriteBlog = (blogs) => {
+  return blogs?.length === 0
+    ? 0
+    : blogs.reduce((accum, blog) => {
+        return blog.likes > accum.likes ? blog : accum;
       });
-  };
+};
+
+const mostBlogs = (blogs) => {
+  return _(blogs)
+    .groupBy('author')
+    .map((items, author) => {
+      return {
+        author: author,
+        blogs: items.length,
+      };
+    })
+    .maxBy((o) => o.blogs)
+};
+
+const mostLikes = (blogs) => {
+  return blogs?.length === 0
+    ? 0
+    : blogs.reduce((accum, blog) => {
+        return blog.likes > accum.likes
+          ? { author: blog.author, likes: blog.likes }
+          : { author: accum.author, likes: accum.likes };
+      });
+};
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
